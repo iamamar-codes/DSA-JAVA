b9b5141a3ce79098777a525a4c96636de4c843a2
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-31 21:44:36 +0530

    Variables in java

--- 06_Basics.java
+++ 06_Basics.java
@@ -1,5 +1,5 @@
 class basics {
-    public static void main(String[] args) {
+    public static void main(String[] args) {     //Entry point of java code- main()
 
         System.out.println("kushwaha" + " Amar");
         System.out.println("4+3");
---
+++ 07_Variables.java
@@ -0,0 +1,17 @@
+class Variables {
+    public static void main(String[] args) {
+        int main = 20;
+        System.out.println(main); // main is not a reserve keyword
+
+
+    }
+}
+
+
+/*
+#RULES FOR VARIABLE NAMING
+
+1. Case Sensitivity[age vs AGE]
+2. Starting Character[a, A,.....,$]
+3. NO Reserved Keywords[class, static etc]
+ */
