d79434c01d69ead8e64e1dac5604edeb0785c27b
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-03 20:46:53 +0530

    2. IF-ELSE Statement

--- 11_Conditionals.java
+++ 11_Conditionals.java
@@ -7,9 +7,19 @@ class Conditionals {
     6. SWITCH Statement */
     public static void main(String[] args) {
         //1. IF - Statement
-        int age = 20;
+        int age = 10;
+
         if(age >= 18){
             System.out.println("You are eligible for Job");
         }
+
+        //2. IF-ELSE Statement
+        if(age >= 18){
+            System.out.println("Your Eligible for Vote");
+        }
+        else{
+            System.out.println("Your Not Eligible for Vote");
+        }
+
     }
 }
