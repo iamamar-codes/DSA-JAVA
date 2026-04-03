4cb18b6ba78de6a25dffbb4cc852611ae5de12b2
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-03 21:38:48 +0530

    Conditionals in Java

--- 11_Conditionals.java
+++ 11_Conditionals.java
@@ -1,115 +1,95 @@
 import java.util.Scanner;
 
 class Conditionals {
-    /*1. IF - Statement
-    2. IF-ELSE Statement
-    3. IF-ELSE-IF Leadder
-    4. NESTED IF-ELSE
-    5. TERNARY OPERATOR
-    6. SWITCH Statement */
+    /*
+     * 1. IF - Statement
+     * 2. IF-ELSE Statement
+     * 3. IF-ELSE-IF Leadder
+     * 4. NESTED IF-ELSE
+     * 5. TERNARY OPERATOR
+     * 6. SWITCH Statement
+     */
     public static void main(String[] args) {
-        //1. IF - Statement
+        // 1. IF - Statement
         int age = 10;
 
-        if(age >= 18){
+        if (age >= 18) {
             System.out.println("You are eligible for Job");
         }
 
-
-        //2. IF-ELSE Statement
-        if(age >= 18){
+        // 2. IF-ELSE Statement
+        if (age >= 18) {
             System.out.println("Your Eligible for Vote");
-        }
-        else{
+        } else {
             System.out.println("Your Not Eligible for Vote");
         }
 
-
-        //3. IF-ELSE-IF Leadder
-        int num1= 20;
-        int num2= 50;
+        // 3. IF-ELSE-IF Leadder
+        int num1 = 20;
+        int num2 = 50;
         int num3 = 40;
 
-        if(num1>num2 && num1>num3){
+        if (num1 > num2 && num1 > num3) {
             System.out.println("num1 is Greater");
-        }
-        else if (num2>num1 && num2>num3){
+        } else if (num2 > num1 && num2 > num3) {
             System.out.println("num2 is Greater");
-        }
-        else{
+        } else {
             System.out.println("num3 is Greater");
         }
 
-
         int day = 16;
 
-        if(day == 1){
+        if (day == 1) {
             System.out.println("Monday");
-        }
-        else if(day == 2){
+        } else if (day == 2) {
             System.out.println("Tuesday");
-        }
-        else if(day == 3){
+        } else if (day == 3) {
             System.out.println("Wednessday");
-        }
-        else if(day == 4){
+        } else if (day == 4) {
             System.out.println("Thursday");
-        }
-        else if(day == 5){
+        } else if (day == 5) {
             System.out.println("Friday");
-        }
-        else if(day == 6){
+        } else if (day == 6) {
             System.out.println("Saturday");
-        }
-        else if(day == 7){
+        } else if (day == 7) {
             System.out.println("Sunday");
-        }
-        else{
+        } else {
             System.out.println("Nothing");
         }
 
-
-
-        //4. NESTED IF-ELSE
+        // 4. NESTED IF-ELSE
         String Surename = "Pandit";
         String subSurename = "Mishra";
 
-
-        if(Surename == "Pandit"){
+        if (Surename == "Pandit") {
             System.out.println("Your are pandit");
-            if(subSurename == "Mishra"){
+            if (subSurename == "Mishra") {
                 System.out.println("Mishra Pandit");
-            }
-            else{
+            } else {
                 System.out.println("Another Pandit");
             }
-        }
-        else{
+        } else {
             System.out.println("You are not a Pandit");
-            if(subSurename == "Mishra"){
+            if (subSurename == "Mishra") {
                 System.out.println("Your not a pandit but Your Mishra");
-            }
-            else{
+            } else {
                 System.out.println("not a pandit not a Mishra");
             }
         }
 
-
-        //5. TERNARY OPERATOR
+        // 5. TERNARY OPERATOR
         int streakDays = 30;
-            //variable                  //if
-        String status = (streakDays >=30) ? "Consistent" : "Irregular";
-                         //condition         //true         //false
+        // variable //if
+        String status = (streakDays >= 30) ? "Consistent" : "Irregular";
+        // condition //true //false
 
         System.out.println(status);
 
-            
         int Age = 19;
-        int ans = (Age>18) ? 22 : 12;
+        int ans = (Age > 18) ? 22 : 12;
         System.out.println(ans);
 
-        
-        //6. SWITCH Statement 
+        // 6. SWITCH Statement
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number: ");
         int dayNumber = sc.nextInt();
@@ -127,13 +107,6 @@ class Conditionals {
             default:
                 System.out.println("Wrong Choice");
         }
-
-
-
-
-
-
-
 
     }
 }
