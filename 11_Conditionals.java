410d27ae7a55c5452c1e581127f186cde8e55505
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-03 20:35:11 +0530

    Conditionals in Java

---
+++ 11_Conditionals.java
@@ -0,0 +1,11 @@
+class Conditionals {
+    /*1. IF - Statement
+    2. IF-ELSE Statement
+    3. IF-ELSE-IF Leadder
+    4. NESTED IF-ELSE
+    5. TERNARY OPERATOR
+    6. SWITCH Statement */
+    public static void main(String[] args) {
+        
+    }
+}
