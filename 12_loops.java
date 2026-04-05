97a8f6ba3a01b379c5d70fe3b1173eca86fc84b9
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-05 19:44:36 +0530

    Loops in JJava

--- 12_loops.java
+++ 12_loops.java
@@ -13,11 +13,29 @@ class loops {
             // System.out.println("Value of i: "+i);
         }
 
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++){
+        for (int i = 1; i <= 3; i++) //Oute Loop
+        { 
+            for (int j = 1; j <= 3; j++) //Inner Loop
+            {
                 System.out.print("* ");
             }
             System.out.println();
         }
+
+        for(int i=1; i<=3; i++){
+            for(int j=1; j<=3; j++){
+                System.out.println("i = " + i + ", j = " + j);
+            }
+            System.out.println();
+        }
+
+        for(int i=1; i<=10; i++){
+            if(i == 5){
+                break;
+            }
+            System.out.println(i);
+        }
+
+        
     }
 }
