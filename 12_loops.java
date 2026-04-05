e645f2fda3f09b6388c631e0ad7c44d6968973e6
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-05 19:07:22 +0530

    Loops in java

---
+++ 12_loops.java
@@ -0,0 +1,24 @@
+class loops {
+    public static void main(String[] args) {
+        // Forloop
+        for (int i = 1; i <= 5; i++) {
+            // System.out.println("Value of i: "+i);
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            // System.out.println("Amar");
+        }
+
+        for (int i = 1; i <= 10; i += 2) {
+            // System.out.println("Value of i: "+i);
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
