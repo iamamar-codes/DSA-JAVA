f8ca590a3155061d2d017d1e974c9e3516d17455
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-05 19:54:24 +0530

    Loops In java

--- 12_loops.java
+++ 12_loops.java
@@ -36,9 +36,12 @@ class loops {
             System.out.println(i);
         }
 
-        // for(){
-
-        // }
+         for(int i=1; i<=10; i++){
+            if(i==5 || i==7 || i==9){
+                continue;
+            }
+            System.out.println(i);
+         }
 
     }
 }
