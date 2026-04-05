07f7f5bcb375a11487c5d264932341288b8e5d88
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-05 19:21:15 +0530

    Loops in Java

--- 12_loops.java
+++ 12_loops.java
@@ -14,11 +14,10 @@ class loops {
         }
 
         for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++) {
+            for (int j = 1; j <= 3; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-
     }
 }
