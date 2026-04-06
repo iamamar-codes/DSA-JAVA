c83ed6fc5738996a88ae14178ea23b742cca6def
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-06 20:33:53 +0530

    print integer range from 50 to 100, that are perfectly divisible by 7

---
+++ 12_divisibleByseven.java
@@ -0,0 +1,9 @@
+class divisibleByseven {
+    public static void main(String[] args) {
+        for(int i=50; i<=100; i++){
+            if(i % 7 == 0){
+                System.out.println("Number is perfectly divisible by 7 : "+ i);
+            }
+        }
+    }
+}
