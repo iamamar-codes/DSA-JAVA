e374b450d540d34160dc3916e828cd66a9863b01
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-07 21:17:36 +0530

    All prime Numbers from 1 to 100

--- 09_primNumbers.java
+++ 09_primNumbers.java
@@ -1,21 +1,19 @@
 class primNumbers {
     public static void main(String[] args) {
-
         int count = 0;
 
-        for(int n = 2; n <= 100; n++) {
-
+        for(int n= 0; n<=100; n++){
             count = 0;
-
-            for(int i = 1; i <= n; i++) {
-                if(n % i == 0) {
-                    count++;
+            for(int i=1; i<=n; i++){
+                if(n % i == 0){
+                    count ++;
                 }
             }
 
-            if(count == 2) {
-                System.out.println(n + " ");
+            if(count == 2){
+                System.out.println("Prime Number from 1 to 100: "+ n);
             }
         }
+        
     }
 }
