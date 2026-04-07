e70ea4ca7d552353f9be00a1baabdc3fa9a5033a
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-07 21:01:49 +0530

    Integers rage from 50 to 100, that are divisible by 7

--- 12_divisibleByseven.java
+++ 12_divisibleByseven.java
@@ -1,9 +1,20 @@
 class divisibleByseven {
     public static void main(String[] args) {
+        //Using for loop
         for(int i=50; i<=100; i++){
             if(i % 7 == 0){
                 System.out.println("Number is perfectly divisible by 7 : "+ i);
             }
+        }
+
+        //Using while loop
+
+        int j = 50;
+        while(j<=100){
+            if(j % 7 == 0){
+                System.out.println("Number is divisible by 7 is " + j);
+            }
+            j++;
         }
     }
 }
