fde297f753fb137071759478915a52f369ca568c
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-09 20:12:55 +0530

    Methods in Java

--- 13_methods.java
+++ 13_methods.java
@@ -1,3 +1,13 @@
 class methods {
-    
+   static void StudentName() {
+        System.out.println("Amar Kushwaha");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Student id: 101");
+        StudentName();
+        System.out.println("Student mobile number: 1234567899");
+
+    }
+
 }
