e2a4814f50c4a2fa17d4523ea934de1c053ddbf3
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-09 17:45:18 +0530

    Solid Squer Patttern

--- 13_solidSquerPattern.java
+++ 13_solidSquerPattern.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 class SquerPettern{
     public static void main(String[] args) {
-        for(int i=1; i<=4; i++){
-            for(int j=1; j<=4; j++){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a single Number for Pattern ");
+        int no = sc.nextInt();
+        
+        for(int i=1; i<=no; i++){
+            for(int j=1; j<=no; j++){
                 System.out.print("* ");
             }
             System.out.println(" ");
