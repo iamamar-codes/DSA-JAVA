5bf4c7dc543b01f93ce9b8adf7de935ba98646d7
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-10 18:11:51 +0530

    Methods in Java

--- 13_methods.java
+++ 13_methods.java
@@ -1,4 +1,5 @@
 class methods {
+    //NO PARAMETERS
    static void add() {
         int a= 2;
         int b =3;
@@ -6,8 +7,14 @@ class methods {
         System.out.println(sum);
     }
 
+    //WITH PARAMETERS
+    static void printSum(int x, int y){
+        System.out.println("Sum: "+ (x + y));
+    }
+
     public static void main(String[] args) {
         add();
+        printSum(20, 5);
     }
 
 }
