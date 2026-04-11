48453707cf373326518626e507f3531748e8914a
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-11 21:11:31 +0530

    Method PrintWelcomeMessage() to print Greeting

---
+++ 16_WelcomeFunction.java
@@ -0,0 +1,11 @@
+class WelcomeFunction {
+
+    static void printWelcomeMessage(){
+        System.out.println("Welcome to the DSA with Java");
+    }
+
+    public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+    
+}
