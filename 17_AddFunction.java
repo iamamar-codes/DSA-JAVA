15cfb4cd7055447b27c5929ebb084944c611ea0a
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-11 21:33:26 +0530

    Create Add() method that return Sum

---
+++ 17_AddFunction.java
@@ -0,0 +1,13 @@
+class AddFunction {
+    static int Add(int a, int b){
+        int sum = a+b;
+        System.out.println("Sum: "+ sum);
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Add(8,9);
+
+    }
+}
