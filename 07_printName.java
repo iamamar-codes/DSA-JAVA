2cde0f77b78d3fc21b586b638d29125dcad65728
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-12 21:12:36 +0530

    Added 14_Array.java

--- 07_printName.java
+++
@@ -1,7 +0,0 @@
-class printName {
-    public static void main(String[] args) {
-        for(int i = 1; i<=100; i++){
-            System.out.println(i+ ") " + "Amar Kushwaha");
-        }
-    }
-}
