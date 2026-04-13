bbb1625e356e89b65a8b6cf36331077af6ee5535
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-13 20:12:50 +0530

    Array in Java

--- 14_Array.java
+++ 14_Array.java
@@ -1,6 +1,17 @@
 class Array {
     public static void main(String[] args) {
-        
+        //Decalarion
+        int arr[];
+        //allocation
+        arr = new int[5];
+
+        //init
+        int brr[] = {10, 20, 30};
+
+        System.out.println("Value at 0 index " + brr[0]);
+        System.out.println("Value at 1 index " + brr[1]);
+        System.out.println("Value at 2 index " + brr[2]);
+
     }
     
 }
