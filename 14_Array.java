362d280292e9af81405217b4ea9d4a8eb5ded5e7
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-13 21:23:11 +0530

    Array Basics in java

--- 14_Array.java
+++ 14_Array.java
@@ -2,51 +2,51 @@ import java.util.Scanner;
 
 class Array {
     public static void main(String[] args) {
-        // //Decalarion
-        // int arr1[];
-        // //allocation
-        // arr1 = new int[5];
+        //Decalarion
+        int arr1[];
+        //allocation
+        arr1 = new int[5];
 
-        // //init
-        // int brr[] = {10, 20, 30};
+        //init
+        int brr[] = {10, 20, 30};
 
-        // System.out.println("Value at 0 index " + brr[0]);
-        // System.out.println("Value at 1 index " + brr[1]);
-        // System.out.println("Value at 2 index " + brr[2]);
+        System.out.println("Value at 0 index " + brr[0]);
+        System.out.println("Value at 1 index " + brr[1]);
+        System.out.println("Value at 2 index " + brr[2]);
 
-        // System.out.println("Print Array Elements using For loop ");
-        // int n1 = 3;
-        // for(int index = 0; index <= n1-1; index++){
-        //     System.out.println(brr[index]);
-        // }
+        System.out.println("Print Array Elements using For loop ");
+        int n1 = 3;
+        for(int index = 0; index <= n1-1; index++){
+            System.out.println(brr[index]);
+        }
 
-        // System.out.println("Print Array Elements using For Each loop ");
-        // for(int val : brr){
-        //     System.out.println(val);
-        // }
+        System.out.println("Print Array Elements using For Each loop ");
+        for(int val : brr){
+            System.out.println(val);
+        }
 
-        // //taking input from user
+        //taking input from user
 
-        // int arr[] = new int[5]; //size of array
-        // Scanner sc = new Scanner(System.in);
-        // int n = arr.length;
-        // for(int i=0; i< n-1; i++){
-        //     System.out.println("Providde inpute for index " + i);
-        //     arr[i] = sc.nextInt();
-        // }
-        // System.out.println("Array Elements: ");
-        // for(int val : arr){
-        //     System.out.print(" " + val);
-        // }
+        int arr[] = new int[5]; //size of array
+        Scanner sc = new Scanner(System.in);
+        int n = arr.length;
+        for(int i=0; i< n-1; i++){
+            System.out.println("Providde inpute for index " + i);
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements: ");
+        for(int val : arr){
+            System.out.print(" " + val);
+        }
 
-        // //1. Practice quetion : sum array list
+        //1. Practice quetion : sum array list
 
-        // int add[] = {10,20,55,30};
-        // int sum = 0;
-        // for(int i=0; i <= add.length-1; i++){
-        //     sum = sum + add[i];
-        // }
-        // System.out.println("Sum of Array : " + sum);
+        int add[] = {10,20,55,30};
+        int sum = 0;
+        for(int i=0; i <= add.length-1; i++){
+            sum = sum + add[i];
+        }
+        System.out.println("Sum of Array : " + sum);
 
         //2. Practice Quetion : Multiply of array List
 
