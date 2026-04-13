b6163ca0b3a5d7a3b2b7be3c3c1109ec4ddbce32
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-14 01:10:55 +0530

    2D Array Basics in java with Practice Quetions

--- 15_2dArray.java
+++ 15_2dArray.java
@@ -3,22 +3,26 @@ class twoDArray {
         // decalartion
         int arr[][];
 
-        // acllocation
+        // allocation
         arr = new int[3][4];
 
         // init
         int brr[][] = {
                 { 1, 2 },
-                { 2, 3 },
-                { 3, 4 }
+                { 2, 3, 7},
+                { 3, 4, 6, 3, 5 },
+                { 4, 5 }
         };
-        //System.out.println(brr[2][1]);
+        // System.out.println(brr[2][1]);
 
         int rowLength = brr.length;
-        int colLength = brr[0].length;
+        // int colLength = brr[0].length; //this use for normal 2d array means same
+        // number of column
 
-        for(int rowIndex=0; rowIndex<=rowLength-1; rowIndex++){
-            for(int colIndex = 0; colIndex <= colLength-1; colIndex++){
+        for (int rowIndex = 0; rowIndex <= rowLength - 1; rowIndex++) {
+
+            int colLength = brr[rowIndex].length;
+            for (int colIndex = 0; colIndex <= colLength - 1; colIndex++) {
                 System.out.print(brr[rowIndex][colIndex] + " ");
             }
             System.out.println(" ");
