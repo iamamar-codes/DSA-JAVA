c98c6e6b18e93b967e1375f61a6198c1e5a2ed41
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-14 20:23:30 +0530

    String in Java

---
+++ 16_String.java
@@ -0,0 +1,14 @@
+ class StringDemo {
+    public static void main(String[] args) {
+        String firstName = "Amar";
+        String lastName = "Kushwaha";
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+
+        System.out.println(lastName.length());
+        System.out.println(firstName.charAt(0));
+        
+
+    }
+}
