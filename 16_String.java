36e26b57bb8560563a07a967fe0524a51cb956e8
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-15 10:47:17 +0530

    String in Java

--- 16_String.java
+++ 16_String.java
@@ -70,13 +70,23 @@ class StringDemo {
 
         //METHODS OF STRING
 
-        //1> .substring(beginIndex, endIndex) : devide string into parts
+        //1> .substring(beginIndex, endIndex): devide string into parts
         //beginIndex ---> Inclusive
         //endIndex ---> Exclusive
         System.out.println(para.substring(2,7));
 
-        //2> .contains() ; check that perticula word is present or not
+        //2> .contains(): check that perticula word is present or not
         System.out.println(para.contains("kushwaha"));
+
+        //3> .valueOf(): change any data type into a String
+        int num = 7345;
+        String change  = String.valueOf(num);
+        System.out.println(num + 1); //7345 + 1 = 7346 (addition) 
+        System.out.println(change + 1); //7345 + 1 = 73451 (concatination)
+
+        //4> .startsWith(String prefix) and .endsWith(String suffix): it checks the string is start or end with that pericular Prefix or Suffix that we enter
+        System.out.println(para.startsWith("My"));
+        System.out.println(para.endsWith("kushwaha"));
 
 
 
