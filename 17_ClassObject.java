5ffba65a0d62afc9b990258996a5da4448a1cd56
iamamar-codes <kushwahaamar104@gmail.com>
2026-04-16 01:39:56 +0530

    String in JavaClasses, Objects, Constructors, Object Lifecycle in JAVA

--- 17_ClassObject.java
+++ 17_ClassObject.java
@@ -4,26 +4,45 @@ class ClassObject {
     int id;
     int age;
 
+    // Default Constructor
+    public ClassObject() {
+        System.out.println("This is Default Constructor");
+    }
+
     // Parameterized Constructor
     public ClassObject(String name, int id, int age) {
+        System.out.println("This is Parameterized Constructor");
         this.name = name;
         this.id = id;
         this.age = age;
     }
 
-    public void Study(){
+    // METHODS OR BEHAVIOURS
+    public void Study() {
         System.out.println("Students are Studying");
     }
-    public void Bunking(){
+
+    public void Bunking() {
         System.out.println("All Students are bunk class today");
     }
 
     public static void main(String[] args) {
-        ClassObject A  = new ClassObject("Amar Kushwaha", 101, 17);
+        ClassObject d = new ClassObject();
+        d.age = 18;
+        d.id = 101;
+        d.name = "Amit";
+        System.out.println(d.age);
+        System.out.println(d.id);
+        System.out.println(d.name);
+        System.out.println();
+
+        ClassObject A = new ClassObject("Amar Kushwaha", 101, 17);
         System.out.println(A.name);
         System.out.println(A.id);
         System.out.println(A.age);
 
+        System.out.println();
+        
         A.Study();
         A.Bunking();
     }
